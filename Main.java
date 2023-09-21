4d13ac04897dca0918105e6737d60e0edddeb52f
Sanjeet76 <3676sanjeetkumargupta@gmail.com>
2023-09-21 18:50:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
     {
         System.out.println("This is java program ");
         System.out.println("Output from java program ");
+        System.out.println("Added one more fire");
     }
 }
